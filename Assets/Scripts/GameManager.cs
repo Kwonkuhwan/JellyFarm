9b910b5df59003dfb62b3ed8a3b73a1f3a840644
Kwon_Gwan <44615788+Kwonkuhwan@users.noreply.github.com>
2023-01-05 19:47:03 +0900

    2023.01.05 - 젤리 성장 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChageAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level-1];
+    }
 }
